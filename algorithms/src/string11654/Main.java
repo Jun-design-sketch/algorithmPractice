2f9ace6bdd82a4ae16fcb61bad73a2049085000a
jeong <jeong.junhueck@gmail.com>
2024-08-16 22:19:42 +0900

    string..

--- algorithms/src/string11654/Main.java
+++ algorithms/src/string11654/Main.java
@@ -1,14 +1,16 @@
 package string11654;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        char ch = (char) br.read();
+        int ascii = (int)ch;
+        br.close();
+        bw.write(""+ascii);
+        bw.flush();
+        bw.close();
     }
 }
