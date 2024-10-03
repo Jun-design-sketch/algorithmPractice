714d2053e1c606ebb5cc0df849642aced1e95593
jeong <jeong.junhueck@gmail.com>
2024-10-04 01:09:17 +0900

    prime number and long

--- algorithms/src/numberTheory/primeNumber4134/Main.java
+++ algorithms/src/numberTheory/primeNumber4134/Main.java
@@ -4,13 +4,85 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // Solution solution = new Solution();
-        // solution.sol1();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
+        for(int i=0; i<n; i++){
+            long answer;
+            long x = Long.parseLong(br.readLine());
+            long j = 0;
+            while(true) {
+                if(x <= 1){
+                    answer = 2;
+                    sb.append(answer+"\n");
+                    break;
+                }
+                if(isPrime(x+j)) {
+                    answer = x+j;
+                    sb.append(answer+"\n");
+                    break;
+                }
+                j++;
+            }
+        }
+        br.close();
+        sb.setLength(sb.length()-1);
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+    }
+    
+    public static boolean isPrime(long x) {
+        for(long j=2; j<=Math.sqrt(x); j++){
+            if(x % j == 0) return false;
+        }
+        return true;
     }
 }
 
 class Solution {
+    protected void sol2() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = Long.parseLong(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<n; i++){
+            if(n == 1){
+                sb.append("2\n");
+                break;
+            }else if(n == 2){
+                sb.append("3\n");
+            }
+            long target = Long.parseLong(br.readLine());
+            while(true){
+                long limit = (int) Math.sqrt(target);
+                boolean flg = false;
+                for(long j=target-1; j>=limit; j--){
+                    if(target % j == 0){
+                        flg = false;
+                        break;
+                    }else{
+                        flg = true;
+                    }
+                }
+                if(flg){
+                    sb.append(target+"\n");
+                    break;
+                }
+                target++;
+            }
+        }
+        br.close();
+        sb.setLength(sb.length()-1);
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+    }
+
     protected void sol1() throws IOException {
         // 各ケースに対して、n以上のうち最小の素数を見つける。
         // nが素数であるかを見つける：n^(-2)まで引き算してみると確認が可能
