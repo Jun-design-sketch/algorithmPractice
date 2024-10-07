924772bba0dda1e2445063b3fd28971b5a93c2c6
jeong <jeong.junhueck@gmail.com>
2024-10-08 00:56:45 +0900

    countPerfectNumber

--- algorithms/src/numberTheory/closeWindows13909/Main.java
+++ algorithms/src/numberTheory/closeWindows13909/Main.java
@@ -1,7 +1,6 @@
 package numberTheory.closeWindows13909;
 
 import java.io.*;
-import java.util.HashMap;
 
 public class Main {
     // 開いている窓の数を求めよ
@@ -10,6 +9,14 @@ public class Main {
     // 2番目の人は２の倍数の窓を閉じる
     // ...
     // 最後に開いている窓の数は？
+
+    // 約数の数が奇数であれば、窓は開く
+
+    // N = 2^a * 3^b + 5*c
+    // (a+1)*(b+1)*(c+1)が約数の数
+    // 掛け算は一つだけでも偶数であれば結果偶数
+    // 全ての素因数の数が偶数でなければ、約数の数は偶数になってしまう。
+    // 全ての素因数の数が偶数ということは、√nは自然数になる
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -18,57 +25,31 @@ public class Main {
         br.close();
 
         int count = 0;
-        for(int i=1; i<=n; i++){
-            HashMap<Integer, Integer> process = countDivisor(i);
-            // TODO: ここから
-            int countDivisor = countPrimeFactor(i)+1;
-            if(countDivisor == 2) count++;
-            else if(countDivisor+1 % 2 == 1) count++;
+        for(int i=1; i*i<=n; i++){
+            count++;
         }
-        // 開いてる窓の数を出力する
         bw.write(String.valueOf(count));
         bw.flush();
         bw.close();
     }
+}
 
-    public static HashMap<Integer, Integer> countDivisor(int target) {
-        if(target == 1){
-            return null;
-        }
-        else{
-            int divisor = 1;
-            HashMap<Integer, Integer> map = new HashMap<>();
-            while(true){
-                divisor++;
-
-                if(target % divisor == 0){
-                    target /= divisor;
-
-                    if(map.containsKey(divisor)) map.put(divisor, map.get(divisor)+1);
-                    else map.put(divisor, 1);
-                    divisor = 1;
-                    if(target == 1) break;
-                }
-            }
-        }
-    }
-
-    public static int countPrimeFactor(int target) {
-        if(target == 1) return 0;
+class Solution {
+    public void sol1() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // 窓の数
+        int n = Integer.parseInt(br.readLine());
+        br.close();
 
         int count = 0;
-        int divisor = 1;
-
-        while(true){
-            divisor++;
-
-            if(target % divisor == 0){
-                target /= divisor;
-                count++;
-                divisor = 1;
-                if(target == 1) break;
-            }
+        for(int i=1; i<=n; i++){
+            double rootD = Math.sqrt(i);
+            double rootI = (int) Math.sqrt(i);
+            if(rootD == rootI) count++;
         }
-        return count;
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
     }
 }
