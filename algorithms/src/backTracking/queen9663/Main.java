afde3f247b99546123f2986e68eebf22aebe1590
jeong <jeong.junhueck@gmail.com>
2024-11-17 16:39:00 +0900

    queen

--- algorithms/src/backTracking/queen9663/Main.java
+++ algorithms/src/backTracking/queen9663/Main.java
@@ -6,11 +6,15 @@ public class Main {
     // 大きさがN＊Nであるチェス板上に、クイーンN個を「お互い攻撃できないよう」配置するとする。
     // クイーンを配置する場合の数を求めよ
     public static int count = 0;
-    public static boolean[][] chessWorld;
+
+    // chessWorld[0]は１番目列を意味し、chessWorld[0]の値が行数を意味するとする
+    public static int[] chessWorld;
+    public static int n;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
         br.close();
 
         // この問題がbackTrackである理由は、１つの置くと
@@ -21,27 +25,48 @@ public class Main {
         // ②(1,1)におかれると (x, 1)は置けない
         // ③(1,1)におかれると (1*x, 1*x)は置けない
 
-        chessWorld = new boolean[n][n];
-        countQueenWorld(n);
+        chessWorld = new int[n];
+        countQueenWorld(0);
 
-        bw.write("...");
+        bw.write(String.valueOf(count));
         bw.flush();
         bw.close();
     }
 
-    public static void countQueenWorld(int n) {
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++){
-                if(chessWorld[i][j]==false){
-                    for(int k=0; k<n; k++){
-                        chessWorld[i][k] = true;
-                        chessWorld[k][j] = true;
-                    }
+    /*
+    ** depth == nになるまで再起呼び出しを行う。
+    ** 再起呼び出しする途中で「もうクイーンを置けない」のであれば呼び出さない
+     */
+    public static void countQueenWorld(int depth) {
+        // 最後の列まで置けたら、countを増やす
+        if(depth == n) {
+            count++;
+            return;
+        }
 
-                    chessWorld[i][j]=true;
-                    countQueenWorld(n);
-                }
+        // depth列の0~(n-1)行に置く場合を数える
+        for(int i=0; i<n; i++){
+            chessWorld[depth] = i;
+            if(Possibility(depth)) {
+                // 列を１ずつ進ませながら置けるかの確認を行う
+                countQueenWorld(depth+1);
             }
         }
+    }
+
+    /*
+    ** 「クイーンをまだ置けるか」の判定を行う。
+     */
+    public static boolean Possibility(int col) {
+        for(int i=0; i<col; i++) {
+            // 値が同じである場合：同じ行に存在する場合
+            if(chessWorld[col] == chessWorld[i]) {
+                return false;
+            // x座標の間隔==y座標の間隔である場合：斜め45度に存在する
+            } else if (Math.abs(col - i) == Math.abs(chessWorld[col] - chessWorld[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 }
