a7c02f315a49260937a24a7ebcb9bd4c673293ea
jeong <jeong.junhueck@gmail.com>
2024-11-23 21:28:27 +0900

    もう少し

--- algorithms/src/backTracking/cal14888/Main.java
+++ algorithms/src/backTracking/cal14888/Main.java
@@ -5,16 +5,14 @@ import java.util.StringTokenizer;
 
 public class Main {
     static public int[] arr;
-    // static public int[] operators = new int[4];
     static public int MAX_VALUE;
     static public int MIN_VALUE;
-    // static public int curVal;
     static public int limit;
+    // 与えられた式の結果での最大値を１行目、最小値を２行目に出力せよ
+    // 数字の順番を変更不可
+    // 演算子の優先順位を無視し、前から演算する
+    // 割り算は定数の割り算で、残りを捨てる。：符号を＋に変えて割り算結果を-に変える
     public static void main(String[] args) throws IOException {
-        // 与えられた式の結果での最大値を１行目、最小値を２行目に出力せよ
-        // 数字の順番を変更不可
-        // 演算子の優先順位を無視し、前から演算する
-        // 割り算は定数の割り算で、残りを捨てる。：符号を＋に変えて割り算結果を-に変える
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         // 数字の数
@@ -47,60 +45,53 @@ public class Main {
         bw.close();
     }
 
-    /*
-    ** 一度使用した数字は再使用できず、数字の順番が変えられないため
-    ** 全ての場合分けを考えると樹形図になり、記号が可変因子
-    ** １つの枝が最後まで進んだら、２段階前に戻り別のケースを確認すればいい
-    ** 3段階前なら？
-     */
-
-    // テストコード
-    public static int count = 0;
-
     public static void eachStep(int curVal, int[] operators, int depth, int i) {
         // 最後まで計算していたら
         if(depth == limit){
             if(MAX_VALUE < curVal) MAX_VALUE = curVal;
             if(MIN_VALUE > curVal) MIN_VALUE = curVal;
-            //テストコード
-            count++;
-            System.out.println(MAX_VALUE+" "+MIN_VALUE+" "+count);
             return;
         }
-        // TODO: 以前段階に戻らせ、他の選択肢を選ばせる => 戻らせるより再帰関数に打ち込んで初期化、また次の再帰関数にする
         // どの演算子を入れるか
         i++;
         if(operators[0] > 0) {
+            int prevVal = curVal;
             curVal += arr[i];
             operators[0]--;
             eachStep(curVal, operators, depth+1, i);
             operators[0]++;
-            curVal = arr[0];
+            // arr[0]に初期化するのではなく直前段階の値に戻す
+            curVal = prevVal;
         }
         if(operators[1] > 0) {
+            int prevVal = curVal;
             curVal -= arr[i];
             operators[1]--;
             eachStep(curVal, operators, depth+1, i);
             operators[1]++;
-            curVal = arr[0];
+            curVal = prevVal;
         }
         if(operators[2] > 0) {
+            int prevVal = curVal;
             curVal *= arr[i];
             operators[2]--;
             eachStep(curVal, operators, depth+1, i);
             operators[2]++;
-            curVal = arr[0];
+            curVal = prevVal;
         }
         if(operators[3] > 0) {
+            int prevVal = curVal;
             if(curVal < 0) {
-                curVal = (curVal * (-1) / arr[i]) * (-1);
+                curVal *= (-1);
+                curVal /= arr[i];
+                if(curVal < 0) curVal *= (-1);
             }else {
-                curVal = curVal / arr[i];
+                curVal /= arr[i];
             }
             operators[3]--;
             eachStep(curVal, operators, depth+1, i);
             operators[3]++;
-            curVal = arr[0];
+            curVal = prevVal;
         }
     }
 }
