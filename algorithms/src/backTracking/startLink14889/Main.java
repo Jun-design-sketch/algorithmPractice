ff4b105b3c872cd5bff4ac2283328b9a41ccb35d
jeong <jeong.junhueck@gmail.com>
2024-11-27 23:16:53 +0900

    ...

--- algorithms/src/backTracking/startLink14889/Main.java
+++ algorithms/src/backTracking/startLink14889/Main.java
@@ -1,6 +1,7 @@
 package backTracking.startLink14889;
 
 import java.io.*;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -24,7 +25,8 @@ public class Main {
         }
         br.close();
 
-        calculator(0, 0);
+        HashSet<Integer> currentMembers = new HashSet<>();
+        calculator(0, 0, currentMembers);
 
         bw.write(String.valueOf(MIN));
         bw.flush();
@@ -34,10 +36,13 @@ public class Main {
     // まず、本人(1)以外の誰か(2)がチームになった場合
     // arr[1][2]+arr[2][1]がチームステータスに足される
     // ここでもう１名(3)足されると。。arr[1][3] arr[3][1] arr[2][3]が足される。。
-    public static void calculator(int value, int depth) {
+    public static void calculator(int value, int depth, HashSet<Integer> currentMembers) {
         if(depth == N/2){
             int teamA = value;
-            int teamB = totalStatus - value;
+
+            // TODO: ここの集計が間違っている。
+            int teamB = otherTeamScore(currentMembers);
+
             int result = Math.abs(teamA-teamB);
             MIN = MIN > result ? result : MIN;
             return;
@@ -46,15 +51,36 @@ public class Main {
         for(int i=0; i<N; i++){
             for(int j=0; j<N; j++) {
                 if(arr[i][j] != 0 && arr[j][i] != 0){
+                    // TODO: こうすると既に２名が加入し、追加で１名入る場合の計算ができない。
                     int tmp1 = arr[i][j];
                     int tmp2 = arr[j][i];
                     arr[i][j] = 0;
                     arr[j][i] = 0;
-                    calculator(value+tmp1+tmp2, depth+1);
+                    currentMembers.add(i);
+                    currentMembers.add(j);
+
+                    calculator(value+tmp1+tmp2, depth+1, currentMembers);
+
+                    currentMembers.remove(i);
+                    currentMembers.remove(j);
                     arr[i][j] = tmp1;
                     arr[j][i] = tmp2;
                 }
             }
         }
+    }
+
+    public static int otherTeamScore(HashSet<Integer> currentMembers) {
+        // 残りのメンバーを抽出する
+        int[] restOfThem = new int[N/2];
+        int count = 0;
+        for(int i=0; i<N; i++){
+            if(!currentMembers.contains(i)){
+                restOfThem[count] = i;
+                count++;
+            }
+        }
+        // 残りのメンバーで可能な組み合わせのスコアの合計をリターンする
+
     }
 }
