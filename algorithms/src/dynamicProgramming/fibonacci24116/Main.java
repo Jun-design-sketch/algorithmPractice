642aa2ddbb269b1c0280ac5c7e7cfdd7194b3cc9
jeong <jeong.junhueck@gmail.com>
2024-12-01 23:27:56 +0900

    実行回数の差

--- algorithms/src/dynamicProgramming/fibonacci24116/Main.java
+++ algorithms/src/dynamicProgramming/fibonacci24116/Main.java
@@ -1,6 +1,10 @@
 package dynamicProgramming.fibonacci24116;
 
+import java.io.*;
+
 public class Main {
+    public static int oldWayCount;
+    public static int dynamicWayCount;
     // Dynamic Programming(DP) 動的計画法
     // 大きい問題を複数の下位問題に分け、各下位問題を解決した結果を保存し（Memoization*）
     // これを繰り返す方法で問題を解決するアルゴリズム設計手法
@@ -15,9 +19,23 @@ public class Main {
     // よく使われる・アクセス時間がかかるデータをメモリに臨時保存し早く使うそれ
     // 速度向上・再利用性・有効性管理（TTL：Time to Live）
     // 大は小を束ねる。Cachingはより包括的なもの
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
 
+        oldWayCount = 0;
+        dynamicWayCount = 0;
+        ordinaryFibonacci(n);
+        dynamicFibonacci(n);
 
+        StringBuilder sb = new StringBuilder();
+        sb.append(oldWayCount).append(" ").append(dynamicWayCount);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 
     // 普通の再帰関数
@@ -28,7 +46,8 @@ public class Main {
     // }
     public static int ordinaryFibonacci(int n) {
         if(n == 1 || n == 2) {
-            return 1;
+            oldWayCount++;
+            return 1;  // code#1
         } else {
             return ordinaryFibonacci(n-1) + ordinaryFibonacci(n-2);
         }
@@ -43,14 +62,15 @@ public class Main {
     // 　return f[n]; f(n)を返却する。
     // }
     public static int dynamicFibonacci(int n) {
-        if(n == 1 || n == 2) {
-            return 1;
-        }
+//        if(n == 1 || n == 2) {
+//            return 1;
+//        }
         int[] f = new int[n+1];
         f[1] = 1;
         f[2] = 1;
-        for(int i=3; i<n; i++) {
-            f[i] = f[i-1]+f[i-2];
+        for(int i=3; i<=n; i++) {
+            dynamicWayCount++;
+            f[i] = f[i-1]+f[i-2]; // #code2
         }
         return f[n];
     }
