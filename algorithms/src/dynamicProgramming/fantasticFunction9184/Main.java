408adc0799ef0c0a01bbc81d4f360c48cd000b10
jeong <jeong.junhueck@gmail.com>
2024-12-04 23:33:20 +0900

    9184..

--- algorithms/src/dynamicProgramming/fantasticFunction9184/Main.java
+++ algorithms/src/dynamicProgramming/fantasticFunction9184/Main.java
@@ -5,27 +5,39 @@ import java.util.StringTokenizer;
 
 public class Main {
 // 再帰関数を考えるとワクワクが止まらない！ではないけれど
-// 以下のような再帰関数 w(a, b, c)があるとする
-// if a <= 0 or b <= 0 or c <= 0, then w(a, b, c) returns: 1
-// if a > 20 or b > 20 or c > 20, then w(a, b, c) returns: w(20, 20, 20)
-// if a < b and b < c, then w(a, b, c) returns: w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
-// otherwise it returns: w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
-// 作るのは簡単だけど実行時間が非常に長くなる。a,b,cが与えられた時 w(a, b, c)を求めよ
+// 動的計画法によってa,b,cが与えられた時 w(a, b, c)を求めよ
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String line;
+        StringBuilder sb = new StringBuilder();
         while((line=br.readLine()) != null) {
             StringTokenizer st = new StringTokenizer(line, " ");
-            doSomething(
-                    Integer.parseInt(st.nextToken()),
-                    Integer.parseInt(st.nextToken()),
-                    Integer.parseInt(st.nextToken())
-            );
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            sb.append("w(").append(a).append(", ");
+            sb.append(b).append(", ");
+            sb.append(c).append(") = ");
+            int d = doSomething(a, b, c);
+            sb.append(d).append("\n");
         }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 
-    public static void doSomething(int a, int b, int c) {
-
+    /*
+    ** でもこれを動的計画法に変えるんだよね？
+     */
+    public static int doSomething(int a, int b, int c) {
+        if(a == 0 || b == 0 || c == 0) return 1;
+        if(a > 20 || b > 20 || c > 20) return doSomething(20, 20, 20);
+        if(a < b && b < c){
+            return doSomething(a,b,c-1)+doSomething(a,b-1,c-1)-doSomething(a,b-1,c);
+        }else{
+            return  doSomething(a-1,b,c) + doSomething(a-1,b-1,c) +
+                    doSomething(a-1,b,c-1) - doSomething(a-1,b-1,c-1);
+        }
     }
 }
