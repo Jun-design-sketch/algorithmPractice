6876fc909f486544c2ca4952f1265dae3a44928d
jeong <jeong.junhueck@gmail.com>
2024-12-06 23:52:41 +0900

    時間超過

--- algorithms/src/dynamicProgramming/fantasticFunction9184/Main.java
+++ algorithms/src/dynamicProgramming/fantasticFunction9184/Main.java
@@ -6,11 +6,13 @@ import java.util.StringTokenizer;
 public class Main {
 // 再帰関数を考えるとワクワクが止まらない！ではないけれど
 // 動的計画法によってa,b,cが与えられた時 w(a, b, c)を求めよ
+    static int[][][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String line;
         StringBuilder sb = new StringBuilder();
+        dp = new int[21][21][21];
         while((line=br.readLine()) != null) {
             StringTokenizer st = new StringTokenizer(line, " ");
             int a = Integer.parseInt(st.nextToken());
@@ -31,8 +33,10 @@ public class Main {
     ** でもこれを動的計画法に変えるんだよね？
      */
     public static int doSomething(int a, int b, int c) {
+        // 以前の結果をキープして効率さを得る
+        if(inRange(a, b, c) && dp[a][b][c] != 0) return dp[a][b][c];
         if(a == 0 || b == 0 || c == 0) return 1;
-        if(a > 20 || b > 20 || c > 20) return doSomething(20, 20, 20);
+        if(a > 20 || b > 20 || c > 20) return dp[20][20][20] = doSomething(20, 20, 20);
         if(a < b && b < c){
             return doSomething(a,b,c-1)+doSomething(a,b-1,c-1)-doSomething(a,b-1,c);
         }else{
@@ -40,9 +44,8 @@ public class Main {
                     doSomething(a-1,b,c-1) - doSomething(a-1,b-1,c-1);
         }
     }
-    // 何かが0になれば1を返す
-    // 何かが20超過なら全部20にして再度返す
-    // a<b<cなら(a,b,c-1)+(a,b-1,c-1)-(a,b-1,c)
-    // a<b<cでなければ？(a-1,b,c-1)+(a-1,b-1,c)+(a-1,b,c-1)-(a-1,b-1,c-1)
-    // そもそも動的計画法とは、以前の結果をキープして効率さを得るもの...
+
+    public static boolean inRange(int a, int b, int c) {
+        return 0 <= a && a <= 20 && 0 <= b && b <= 20 && 0 <= c && c <= 20;
+    }
 }
