25a1f0c45787ba31454c7e58b14d939469987c6a
jeong <jeong.junhueck@gmail.com>
2025-02-18 00:31:20 +0900

    1149

--- algorithms/src/dynamicProgramming/rgbDistance1149/Main.java
+++ algorithms/src/dynamicProgramming/rgbDistance1149/Main.java
@@ -12,17 +12,19 @@ public class Main {
     // ①1番家の色は2番家の色と同じでないこと。
     // ②N番家の色はN-1番家の色と同じでないこと。
     // ③i(2<=i<=N-1)番家の色はi-1, i+1番家の色と同じでないこと。
+    public static int n;
     public static int[][] arr;
+    public static int[] ans;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        arr = new int[3][n];
+        n = Integer.parseInt(br.readLine());
+        arr = new int[3][n+1];
 
-        for(int i=0; i<n; i++){
+        for(int i=1; i<n+1; i++){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for(int j=0; j<3; j++){
+            for(int j=1; j<4; j++){
                 arr[j][i] = Integer.parseInt(st.nextToken());
             }
         }
@@ -38,6 +40,24 @@ public class Main {
     public static void doSomething() {
         // 以前と以降の家とは違う色を塗る時、費用の全量が最小限になる方法。。
         // DPに入ってからあまり捗らなず
+        ans = new int[n];
+        boolean[] bol = new boolean[3];
 
+        for(int i=1; i<n+1; i++) {
+            int a = arr[1][i];
+            int b = arr[2][i];
+            int c = arr[3][i];
+//            int min = Math.min(a, Math.min(b, c));
+//            if (i % 3 == 1){
+//
+//                if(min == a) { ans[i] = arr[1][i];
+//                else if(min == b) { ans[i] = arr[2][i]; }
+//                else { ans[i] = arr[3][i]; }
+//            }
+            // TODO: 最初R,G,Bのどれかを選択することによって以降の最初値が決まるので場合分けの漸化式
+            // ans[1][0] = arr[1][0]であれば
+            // ans[2][0] += min(arr[1][1], arr[1][2]),
+            // ...
+        }
     }
 }
