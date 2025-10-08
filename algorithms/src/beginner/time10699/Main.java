4f49dee3866f9f7d0febe5872d5174405295c3f8
jeong <jeong.junhueck@gmail.com>
2025-10-09 00:03:00 +0900

    ...

---
+++ algorithms/src/beginner/time10699/Main.java
@@ -0,0 +1,24 @@
+package beginner.time10699;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Instant: UTC"); // Instant.now()は常にUTC
+        Instant UTC = Instant.now();
+        Duration nineHour = Duration.ofHours(9);
+        Instant plusNine = UTC.plus(nineHour);
+        String today = plusNine.toString().substring(0, 10);
+        System.out.println(today);
+
+        // ZoneIdで地域指定、ZoneDateTimeを作る
+        System.out.println("Instant: ZoneId");
+        Instant nowUtc = Instant.now();
+        ZonedDateTime tokyo = nowUtc.atZone(ZoneId.of("Asia/Tokyo"));
+        String today2 = tokyo.toLocalDate().toString();
+        System.out.println(today2);
+    }
+}
