2828269b66e281be644668160c370be6c5c20316
jeong <jeong.junhueck@gmail.com>
2026-05-29 00:02:53 +0900

    leet0219 review

--- algorithms/src/leet/leet0219/Main.java
+++ algorithms/src/leet/leet0219/Main.java
@@ -1,6 +1,7 @@
 package leet.leet0219;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,11 +20,20 @@ public class Main {
     }
 
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // 具現体のHashMapより、抽象タイプMapで扱う方が望ましい
+        // 具現体の特徴が必須なら使ってもよいが、型変換や汎用性で見てMap機能のみなら左辺はMapの方が柔軟
+        // （左）何ができるか = （右）具体的にどうするか
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // O(n)
         for(int i = 0; i < nums.length; i++) {
+            // O(1)
             if(map.containsKey(nums[i]) && (i - map.get(nums[i])) <= k) return true;
+            // この問題では測定済みかつここに辿りづいた時点で再度更新してOKになる
             map.put(nums[i], i);
         }
         return false;
+        // 全ての要素がMAPに独立して入ると空間複雑度はO(n)
     }
 }
