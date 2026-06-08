f76bcff748e5f51ba91cb385be2533a09f51e74f
jeong <jeong.junhueck@gmail.com>
2026-06-08 17:15:49 +0900

    leet0345 review

--- algorithms/src/leet/leet0345/Main.java
+++ algorithms/src/leet/leet0345/Main.java
@@ -13,22 +13,36 @@ public class Main {
     }
 
     // 母音だけ洗い出して文字列内で位置をひっくり返す
+    // 今度two pointerでやってみる
     public String reverseVowels(String s) {
         Set<Integer> vowels = new HashSet<>(Arrays.asList(65, 69, 73, 79, 85, 97, 101, 105, 111, 117));
+
+        // Characterを直接入れても良い
+        // Set<Character> vowels = new HashSet<>(
+        //  Arrays.asList('a', 'e', 'i', 'o', 'u','A', 'E', 'I', 'O', 'U')
+        // );
+
+        // Setにせず、Stringでも判定できる
+        // private boolean isVowel(char ch) {
+        //  return "aeiouAEIOU".indexOf(ch) >= 0;
+        // }
+
         Deque<Character> stack = new ArrayDeque<>();
-        int[] indexes = new int[s.length()];
+        // int[]でなくとも、boolean[]で十分
+        // int[] indexes = new int[s.length()];
+        boolean[] isVowelIndex = new boolean[s.length()];
 
         for(int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if(vowels.contains((int) ch)) {
-                indexes[i] = 1;
+                isVowelIndex[i] = true;
                 stack.push(ch);
             }
         }
 
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < s.length(); i++) {
-            if(indexes[i] != 1) sb.append(s.charAt(i));
+            if(isVowelIndex[i] != true) sb.append(s.charAt(i));
             else sb.append(stack.pop());
         }
 
