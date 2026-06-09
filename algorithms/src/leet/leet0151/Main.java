2cfdb98fd2950ca4d99a991e311b0fcebcc4325e
jeong <jeong.junhueck@gmail.com>
2026-06-09 22:16:53 +0900

    leet0151

---
+++ algorithms/src/leet/leet0151/Main.java
@@ -0,0 +1,19 @@
+package leet.leet0151;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        String q1 = "the sky is blue";
+        System.out.println(main.reverseWords(q1)); // "blue is sky the"
+
+        String q2 =  " hello world  ";
+        System.out.println(main.reverseWords(q2)); // "world hello"
+
+        String q3 = "a good   example";
+        System.out.println(main.reverseWords(q3)); // "example good a"
+    }
+
+    public String reverseWords(String s) {
+
+    }
+}
