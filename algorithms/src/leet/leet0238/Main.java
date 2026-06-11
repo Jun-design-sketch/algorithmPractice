de93aaca14e8e83d225137fec61d97b8d9eaf8a7
jeong <jeong.junhueck@gmail.com>
2026-06-12 00:05:15 +0900

    leet0238

---
+++ algorithms/src/leet/leet0238/Main.java
@@ -0,0 +1,16 @@
+package leet.leet0238;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        int[] nums1 = {1,2,3,4};
+        System.out.println(main.productExceptSelf(nums1)); // 24,12,8,6
+
+        int[] nums2 = {-1,1,0,-3,3};
+        System.out.println(main.productExceptSelf(nums1)); // 0,0,9,0,0
+    }
+
+    public int[] productExceptSelf(int[] nums) {
+
+    }
+}
