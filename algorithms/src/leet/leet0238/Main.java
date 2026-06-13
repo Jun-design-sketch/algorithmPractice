142a724f0d19782a622dda72472f5fa67416cc8b
jeong <jeong.junhueck@gmail.com>
2026-06-13 10:35:07 +0900

    leet0238: blueprint

--- algorithms/src/leet/leet0238/Main.java
+++ algorithms/src/leet/leet0238/Main.java
@@ -1,16 +1,35 @@
 package leet.leet0238;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         int[] nums1 = {1,2,3,4};
-        System.out.println(main.productExceptSelf(nums1)); // 24,12,8,6
+        System.out.println(Arrays.toString(main.productExceptSelf(nums1))); // 24,12,8,6
 
         int[] nums2 = {-1,1,0,-3,3};
-        System.out.println(main.productExceptSelf(nums1)); // 0,0,9,0,0
+        System.out.println(Arrays.toString(main.productExceptSelf(nums2))); // 0,0,9,0,0
     }
 
     public int[] productExceptSelf(int[] nums) {
+        int[] left = new int[nums.length];
+        for(int i=0; i<nums.length; i++) {
+            if(i == 0) left[i] = 1;
+            else left[i] = left[i-1] * nums[i-1];
+        }
 
+        int[] right = new int[nums.length];
+        for(int i=nums.length-1; i>=0; i--) {
+            if(i == nums.length-1) right[i] = 1;
+            else right[i] = right[i+1] * nums[i+1];
+        }
+
+        int[] answers = new int[nums.length];
+        for(int i=0; i<nums.length; i++) {
+            answers[i] = left[i] * right[i];
+        }
+
+        return answers;
     }
 }
