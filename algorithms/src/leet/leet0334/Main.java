c3a99abe84d3dacc4caf1037a470fed67d078dc4
jeong <jeong.junhueck@gmail.com>
2026-06-16 00:39:11 +0900

    leet0334:

---
+++ algorithms/src/leet/leet0334/Main.java
@@ -0,0 +1,20 @@
+package leet.leet0334;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        int[] nums1 = {1,2,3,4,5};
+        System.out.println(main.increasingTriplet(nums1)); // true
+
+        int[] nums2 = {5,4,3,2,1};
+        System.out.println(main.increasingTriplet(nums2)); // false
+
+        int[] nums3 = {2,1,5,0,4,6};
+        System.out.println(main.increasingTriplet(nums3)); // true
+    }
+
+    public boolean increasingTriplet(int[] nums) {
+
+        return false;
+    }
+}
